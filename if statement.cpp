c05ddf6c40ceedbc259acf9c90a4c596fe430b2e
Saad <saadbinmontasir11@gmail.com>
2024-08-26 22:46:36 +0600

    Add files via upload

---
+++ if statement.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int num;
+cout<<" enter any number:";
+cin>>num;
+
+if( num>0)
+{ cout <<" positive number it is";}
+
+if(num<0)
+{ cout<<" then it is negative number";}
+
+if ( num==0)
+{cout<< "then iwill be happy";}
+
+cout<<" aurin";
+     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
