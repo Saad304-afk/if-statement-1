a209a098091a027a869ce3e8de6312d8a885e418
Saad <saadbinmontasir11@gmail.com>
2024-10-11 11:55:47 +0600

    Add files via upload

---
+++ nested if .cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int mark ;
+
+cout<<" enter your number:";
+cin>> mark;
+
+if( mark >32){
+     cout<<" you are pass";
+    if(  mark>80)
+    {
+    cout<< "your result is A+";
+    }
+    else if ( mark>70)
+    {
+        cout << "your result is A";
+    } }
+
+else {  cout<<" sorry , better luck next time , you are fail";}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
