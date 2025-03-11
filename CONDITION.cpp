8c57f93aa06ffb0324c9b552a9e65e832954ab95
Saad <saadbinmontasir11@gmail.com>
2025-03-11 23:04:41 +0600

    Add files via upload

---
+++ CONDITION.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+int main()
+{
+    int A,B;
+    cin>>A>>B;
+    if(A>=B)
+
+        cout<<"Yes";
+    else
+    cout<<"No";
+ return 0;}
+
+
+
